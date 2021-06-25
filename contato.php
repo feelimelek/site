a799beb8d5d248fa52042b4a1f7d04e1bde7aa50
Fernanda Elimelek <fernanda.elimelek@usp.br>
2021-06-25 08:42:18 -0300

    my website

---
+++ contato.php
@@ -0,0 +1,16 @@
+<div id="main">
+	<span style="color: black">- CONTATO </span>
+	<h2> Caso tenham algum comentário, entrem em contato comigo </h2>
+</div>
+<div id="contato">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-sm-12">
+				<h3>E-mail: fe.elimelek@avanhandava.org</h3>
+				<h3>Facebook: Fernanda Elimelek</h3>
+				<h3>Instagram: fe_elimelek</h3>
+				<h3>Celular: 11986352202</h3>
+			</div>
+		</div>
+	</div>
+</div>
